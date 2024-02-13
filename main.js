70b1344d5604b29707f0500c94f6619a9200a76f
Christian <80511020+Dracothehuman@users.noreply.github.com>
2024-02-13 21:06:33 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+var wee="";
+
+
+
+function duck(){
+    wee="duck";
+}
+function tennis(){
+    wee="tennis";
+}
+function rock(){
+    wee="rock";
+}
+function man(){
+    wee="superman";
+}
+function doom(){
+    wee="doom";
+}
+function gow(){
+    wee="kratos";
+}
+function goku(){
+    wee="goku";
+}
+function gojo(){
+    wee="gojo";
+}
+
+function fight(){
+    if(wee=="duck"){
+        window.location="duck.html";
+    }
+    else if(wee=="tennis"){
+        window.location="tennis.html";
+    }
+    else if(wee=="rock"){
+        window.location="rock.html";
+    }
+    else if(wee=="superman"){
+        window.location="superman.html";
+    }
+    else if(wee=="doom"){
+        window.location="doom.html";
+    }
+    else if(wee=="kratos"){
+        window.location="kratos.html";
+    }
+    else if(wee=="goku"){
+        window.location="goku.html"; 
+    }
+    else if(wee=="gojo"){
+        window.location="gojo.html";
+    }
+}
+
